ddc00f1d44108a2216ed41cefecbde4efa5a5b0e
Aakash Maroti <aakash94@users.noreply.github.com>
2022-11-18 00:25:08 +0530

    working example

--- SimpleScraper.py
+++ SimpleScraper.py
@@ -2,11 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import os.path
 
-web_prefix = "http:"
-site = "http://webaddress.com/link/to/webpage"
-path = "D:\\Path\\to\\Folder\\Scrapped\\"
+web_prefix = "http://rail.eecs.berkeley.edu/"
+site = "http://rail.eecs.berkeley.edu/deeprlcourse-fa20/"
+path = "D:\\Study\\UCB\\CS285\\2020\\Slides\\"
 
-extension = ".xyz"
+extension = ".pdf"
 num_ignore_char = 0
 #number of characters to remove from the start of the extracted url
 
@@ -16,7 +16,7 @@ def download_link(download_url, store_path):
         return  True
     with open(store_path, 'wb') as f:
         if 'http' not in download_url:
-            # sometimes an image source can be relative
+            # sometimes a file source can be relative
             # if it is provide the base url which also happens
             # to be the site variable atm.
             download_url = '{}{}'.format(web_prefix, download_url)
@@ -26,23 +26,30 @@ def download_link(download_url, store_path):
 
 def get_urls(site):
     urls = []
+    names = []
     response = requests.get(site)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     all_links = soup.find_all("a")
     for link in all_links:
-        if extension in link.get("href"):
-            urls.append(link.get("href")[num_ignore_char:])
-    return urls
+        href = link.get("href")
+        if href == None:
+            continue
+        if extension in href:
+            url = href[num_ignore_char:]
+            urls.append(url)
+            name = link.string.replace(':', '.')
+            names.append(name)
+    return list(zip(urls, names))
 
 
-urls = get_urls(site)
-count = 0
-total = len(urls)
-for url in urls:
-    filename = url.split('/')[-1]
-    filename = path+filename
-    print("{0}/{1}\t{2}\t{3}".format(count, total, url, filename))#, end='\r')
-    count+=1
-    if(not download_link(url, filename)):
-        print("\t\tError downloading ", url, " into ", filename)
+if __name__ == "__main__":
+    urls = get_urls(site)
+    count = 0
+    total = len(urls)
+    for url, name in urls:
+        filename = path+name+extension
+        print("{0}/{1}\t{2}\t{3}".format(count, total, url, filename))#, end='\r')
+        count+=1
+        if(not download_link(url, filename)):
+            print("\t\tError downloading ", url, " into ", filename)
